c48d4daae2eea5b0df9e790fce7c1909ff6ed781
ajitlenka30 <ajitlenka30@gmail.com>
2021-12-21 08:57:13 +0000

    sweet: wellcome cherish os

--- cherish_sweet.mk
+++ cherish_sweet.mk
@@ -5,21 +5,26 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit common Nusantara configurations
-$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
-NAD_BUILD_TYPE := OFFICIAL
-TARGET_BOOT_ANIMATION_RES := 1080
-USE_PIXEL_CHARGING := true
-TARGET_USES_BLUR := true
-TARGET_GAPPS_ARCH := arm64
+# Inherit common Cherish OS configurations
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
-PRODUCT_NAME := nad_sweet
+PRODUCT_NAME := cherish_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Cherish OS Stuffs
+CHERISH_BUILD_TYPE := OFFICIAL
+WITH_GMS := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+
+# Maintainer
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.cherish.maintainer=AJIT
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="dipper-user 8.1.0 OPM1.171019.011 V9.5.5.0.OEAMIFA release-keys"
