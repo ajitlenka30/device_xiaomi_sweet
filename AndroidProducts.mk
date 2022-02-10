9908b9397920dbcd9896e6dffacaeed458ab61ca
ajitlenka30 <ajitlenka30@gmail.com>
2022-02-10 08:27:03 +0000

    sweet: wellcome xdroid

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/cherish_sweet.mk
+    $(LOCAL_DIR)/xdroid_sweet.mk
 
 COMMON_LUNCH_CHOICES := \
-    cherish_sweet-eng \
-    cherish_sweet-userdebug \
-    cherish_sweet-user
+    xdroid_sweet-eng \
+    xdroid_sweet-userdebug \
+    xdroid_sweet-user
--- xdroid_sweet.mk
+++ xdroid_sweet.mk
@@ -5,10 +5,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit common Cherish OS configurations
-$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
+# Inherit common xdroidsp configurations
+$(call inherit-product, vendor/xdroid/config/common.mk)
 
-PRODUCT_NAME := cherish_sweet
+PRODUCT_NAME := xdroid_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10 Pro
@@ -16,15 +16,13 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-# Cherish OS Stuffs
-CHERISH_BUILD_TYPE := OFFICIAL
-WITH_GMS := true
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm64
-
-# Maintainer
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.cherish.maintainer=AJIT
+# xdroidsp stuff
+XDROID_BOOT := 1080
+XDROID_MAINTAINER := AJIT
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+TARGET_INCLUDE_STOCK_ARCORE := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_SUPPORTS_QUICK_TAP := true
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="dipper-user 8.1.0 OPM1.171019.011 V9.5.5.0.OEAMIFA release-keys"
