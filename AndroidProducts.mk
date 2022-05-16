0d5ee8c44af569f029ccc2fac30b157cdf9a0ff9
ajitlenka30 <ajitlenka30@gmail.com>
2022-05-16 09:51:08 +0000

    sweet: wellcome Spark OS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/cherish_sweet.mk
+    $(LOCAL_DIR)/spark_sweet.mk
 
 COMMON_LUNCH_CHOICES := \
-    cherish_sweet-eng \
-    cherish_sweet-userdebug \
-    cherish_sweet-user
+    spark_sweet-eng \
+    spark_sweet-userdebug \
+    spark_sweet-user
--- spark_sweet.mk
+++ spark_sweet.mk
@@ -5,10 +5,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit common Cherish OS configurations
-$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
+# Inherit common Spark OS configurations
+$(call inherit-product, vendor/spark/config/common_full_phone.mk)
 
-PRODUCT_NAME := cherish_sweet
+PRODUCT_NAME := spark_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10 Pro
@@ -16,15 +16,12 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-# Cherish OS Stuffs
-CHERISH_BUILD_TYPE := OFFICIAL
+# Spark OS Stuffs
 WITH_GMS := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
-
-# Maintainer
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.cherish.maintainer=AJIT
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="dipper-user 8.1.0 OPM1.171019.011 V9.5.5.0.OEAMIFA release-keys"
